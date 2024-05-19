f079187061a41bdd2cf474ec7f89038f48c65334
Gibby Free <gib@gibby.dev>
2024-05-19 10:08:48 -0700

    layout done

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,7 +1,6 @@
 // place files you want to import through the `$lib` alias in this folder.
 import { writable } from 'svelte/store';
 
-export const grid = writable(Array(20).fill(Array(10).fill(0))); // 20x10 grid
 export const pieces = writable([
     { id: 1, type: 'I', shape: [[0, 0, 0, 0], [1, 1, 1, 1], [0, 0, 0, 0], [0, 0, 0, 0]], x: 0, y: 0 },
     { id: 2, type: 'O', shape: [[0, 0, 0, 0], [0, 1, 1, 0], [0, 1, 1, 0], [0, 0, 0, 0]], x: 0, y: 0 },
